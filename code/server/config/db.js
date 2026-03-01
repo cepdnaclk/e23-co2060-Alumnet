b07772862f2750a79ac530c73f4d733b5332c881
Enuri <enuriwickramasinghe@gmail.com>
2026-03-01 15:00:34 +0530

    Log whether DATABASE_URL is set on startup

--- code/server/config/db.js
+++ code/server/config/db.js
@@ -1,5 +1,8 @@
 require("dotenv").config(); 
 
+console.log("DATABASE_URL set?", Boolean(process.env.DATABASE_URL));
+console.log("DB_HOST is", process.env.DB_HOST);
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
