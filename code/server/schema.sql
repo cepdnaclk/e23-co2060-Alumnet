2babceb81eb3193d3256352d67235b52b0bc12dc
senath08 <e23362@eng.pdn.ac.lk>
2026-06-25 07:36:58 +0530

    Add the new table Notificatio

--- code/server/schema.sql
+++ code/server/schema.sql
@@ -27,3 +27,13 @@ CREATE TABLE events (
     created_by UUID REFERENCES users(id) ON DELETE SET NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE notifications (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES alumni_profiles(user_id) ON DELETE CASCADE,
+    title VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    is_read BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
