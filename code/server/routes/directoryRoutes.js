ff2c43b9a7f869d73803f531570a4ad88bb75bc8
Enuri Wickramasinghe <enuriwickramasinghe@gmail.com>
2026-07-19 13:34:09 +0530

    Add alumni recommendation system and redesign directory interface

--- code/server/routes/directoryRoutes.js
+++ code/server/routes/directoryRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const {
   getAlumniDirectory,
+  getRecommendedAlumni,
   getPublicAlumniProfile,
 } = require("../controllers/directoryController");
+const { protect, authorize } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.get("/", getAlumniDirectory);
+router.get("/recommended", protect, authorize("student"), getRecommendedAlumni);
 router.get("/:id", getPublicAlumniProfile);
 
 module.exports = router;
